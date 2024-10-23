3d05b4932e10d44a2a96b23e1be1de58f6519695
BrunoPassuello <bruno.fogazzi.passuello@gmail.com>
2024-10-23 11:39:38 -0300

    Sequencia de Fibonacci recursiva e linear

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        try {
+            System.out.println(fibonaccirecursiva(6));
+            System.out.println(fibonaccilinear(6));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public static int fibonaccirecursiva(int n) throws Exception {
+            if (n < 0) {
+                throw new Exception("A entrada deve ser maior ou igual a 0");
+            }
+            if (n == 0 || n == 1) {
+                return n;
+            }
+
+            return fibonaccirecursiva(n - 1) + fibonaccirecursiva(n - 2);
 
     }
+
+    public static int fibonaccilinear(int n) throws Exception{
+        if (n < 0) {
+            throw new Exception("A entrada deve ser maior ou igual a 0");
+        }
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+
 }
